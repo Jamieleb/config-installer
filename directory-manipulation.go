d6e033c431e81e306810fdf4d44c0ae4a066a3b4
Jamie Le Brun <j.lebrun9@gmail.com>
2021-04-06 05:56:11 +0100

    refac: moved more directory and file func

--- directory-manipulation.go
+++ directory-manipulation.go
@@ -1,8 +1,11 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
-func (ps filepaths) removeFilesOrDirs(targetDir string) []error {
+func (ps pathList) removeFilesOrDirs(targetDir string) []error {
   errors:= []error{}
 
   for _, p := range ps {
@@ -11,4 +14,29 @@ func (ps filepaths) removeFilesOrDirs(targetDir string) []error {
   }
 
   return errors
+}
+
+func (dirs directories) create(targetDir string) {
+  for _, d := range dirs {
+    err := os.MkdirAll(targetDir + "/" + d, 0755)
+    if err != nil {
+      fmt.Println("Error:", err)
+      os.Exit(1)
+    }
+  }
+}
+
+func (files filepaths) createSymLinks(src string, target string) []error {
+  errors := []error{}
+
+  for _, f := range files {
+    err := os.Symlink(getFullPath(src, f), getFullPath(target, f))
+    if err != nil { errors = append(errors, err) }
+  }
+
+  return errors
+}
+
+func getFullPath(base string, filepath string) string {
+  return base + "/" + filepath
 }
